1dc04692019d801516bd9764982127f5db58c372
thiswillbeyourgithub <github@32mail.33mail.comm>
2021-07-26 21:18:44 +0200

    fix: removes old file

--- IndexableAnki.py
+++ IndexableAnki.py
@@ -169,6 +169,7 @@ for i in tqdm(db.index):
     save_card_as_file(i)
 
 print("Compressing as a zip archive...")
+os.system(f"rm -r {args['output_dir']}/IndexableAnki_{args['profile_escaped']}.zip")
 os.system(f"cd /tmp/IndexableAnki && zip -9 {args['output_dir']}/IndexableAnki_{args['profile_escaped']}.zip ./*")
 
 
